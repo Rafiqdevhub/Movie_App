b9d48e666dfeab5e7ec9cf1b217e7c820510662d
Muhammad Rafiq <rafkhan9323@gmail.com>
2025-01-26 15:29:03 +0500

    Refactor search term handling in updateSearchCount function

--- src/appwrite.js
+++ src/appwrite.js
@@ -13,20 +13,18 @@ const database = new Databases(client);
 export const updateSearchCount = async (searchTerm, movie) => {
   try {
     const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
-      Query.equal("searchTerm", searchTerm),
+      Query.equal("search", searchTerm),
     ]);
 
-    // 2. If it does, update the count
     if (result.documents.length > 0) {
       const doc = result.documents[0];
 
       await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
         count: doc.count + 1,
       });
-      // 3. If it doesn't, create a new document with the search term and count as 1
     } else {
       await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
-        searchTerm,
+        search: searchTerm,
         count: 1,
         movie_id: movie.id,
         poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
